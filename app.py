05c3d64574cb52109fed04016563a24159579e07
Alysson <alyssos@hotmail.com>
2024-07-09 16:51:17 -0300

    feat: api with llama3

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ from langchain_community.llms import Ollama
 
 app = Flask(__name__)
 
-# llm = Ollama(model="llama3")
-
-# respose = llm.invoke("tell me a cat joke")
-
-# print(respose)
+cached_llm = Ollama(model="llama3")
 
 def start_app():
     app.run(host="0.0.0.0", port = 8080 , debug = True)
 
 @app.route("/ai", methods=["POST"])
 def aipost():
-    print("post /ai called")
+    print("Post /ai called")
     json_content = request.json
     query = json_content.get("query")
 
     print(f"query: {query}")
 
-    response_answer = "sample response. query: "+ query
+    respose = cached_llm.invoke(query)
+
+    print(respose)
+
+    response_answer = {"answer": respose}
     return response_answer
 
 if __name__ == "__main__":
